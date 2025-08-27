8fa883c28ac78870a0c453d87bef7e7738948e25
Luis <canito367@gmail.com>
2025-08-27 15:38:12 -0400

    Migracion a postgres

--- config.py
+++ config.py
@@ -1,6 +1,13 @@
 import os
 
 class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY', 'supersecreto')  
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///database.db')
+    SECRET_KEY = os.getenv("SECRET_KEY", "supersecreto")
+    
+    DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///database.db")
+    
+    if DATABASE_URL.startswith("postgres://"):
+        DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql+psycopg2://", 1)
+    
+    SQLALCHEMY_DATABASE_URI = DATABASE_URL
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    
